e525859fa8fb914f1cd7b2f4601ac18909ec70d0
Nahidul Islam Siam <siamnahidul093@gmail.com>
2024-06-24 16:10:52 +0600

    50% complete

---
+++ src/app/components/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hero from './Hero';
+import PreviousWork from './PreviousWork';
+
+const Homepage = () => {
+    return (
+        <div>
+            <Hero/>
+            <PreviousWork/>
+        </div>
+    );
+};
+
+export default Homepage;
